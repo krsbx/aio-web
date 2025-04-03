d93785c4954c02567f90f9afa3b2b21f588c6a0b
Muhammad Firdaus Sati <firdaus.sati51@gmail.com>
2025-04-03 10:56:42 +0700

    feat(securedb): sql securedb

--- packages/securedb/src/sql/index.ts
+++ packages/securedb/src/sql/index.ts
@@ -1,81 +1,139 @@
-import { Database, type Column, type Table } from '@ignisia/sql';
+import { type Column, Database, QueryBuilder, type Table } from '@ignisia/sql';
 import { Dialect } from '@ignisia/sql/dist/table/constants';
 import type { DefineSecureDbOptions, SecureDbOptions } from './types';
-import { isFileExists, readMeta, writeMeta } from '../utilities';
-import { decryptFile } from '@ignisia/encryption';
 import type { DatabaseMeta } from '../types';
+import { isFileExists, readMeta, writeMeta } from '../utilities';
+import { decryptFile, encryptFile } from '@ignisia/encryption';
+import type { DatabaseOptions } from '@ignisia/sql/dist/database/types';
 
 export class SecureSqlDb<
   DbDialect extends typeof Dialect.SQLITE,
   Tables extends Record<string, Table<string, Record<string, Column>>>,
-> {
-  public db: Database<DbDialect, Tables>;
-  private decryptedFilePath: string;
-  private encryptedFilePath: string;
-  public metaPath: string;
-  public meta: DatabaseMeta;
-  public password: string;
-  public salt: string | null;
+> extends Database<DbDialect, Tables> {
+  protected decryptedFilePath: string;
+  protected encryptedFilePath: string;
+  protected password: string;
+  protected salt: string | null;
+  protected isProtected: boolean;
+  public readonly metaPath: string;
+  public readonly meta: DatabaseMeta;
 
-  protected constructor(options: SecureDbOptions<Database<DbDialect, Tables>>) {
-    if (options.db.dialect !== Dialect.SQLITE) {
-      throw new Error('Only SQLite is supported');
-    }
+  protected constructor(options: SecureDbOptions<DbDialect, Tables>) {
+    super(options);
 
-    this.db = options.db;
     this.password = options.password;
     this.decryptedFilePath = options.decryptedFilePath;
     this.encryptedFilePath = options.encryptedFilePath;
     this.metaPath = options.metaPath;
     this.meta = options.meta;
     this.salt = options.salt ?? null;
+    this.isProtected = !!options.password;
   }
 
-  public static async define<
+  protected async trackChange() {
+    this.meta.totalChanges++;
+
+    if (this.meta.totalChanges % 5 === 0) {
+      await encryptFile({
+        input: this.decryptedFilePath,
+        output: this.encryptedFilePath,
+        salt: this.salt,
+        password: this.password,
+      });
+
+      this.meta.lastEncryptedAt = new Date();
+      this.meta.totalChanges = 0;
+    }
+
+    await writeMeta(this.metaPath, this.meta);
+  }
+
+  public table<
+    TableName extends keyof Tables & string,
+    Table extends Tables[TableName],
+  >(tableName: TableName) {
+    const query = super.table(tableName);
+    const exec = query.exec;
+
+    query.exec = async () => {
+      const result = exec.call(query);
+
+      if (this.isProtected) {
+        await this.trackChange();
+      }
+
+      return result;
+    };
+
+    return query as unknown as QueryBuilder<TableName, Table>;
+  }
+
+  public static define<
     DbDialect extends typeof Dialect.SQLITE,
     Tables extends Record<string, Table<string, Record<string, Column>>>,
-  >(options: DefineSecureDbOptions<DbDialect, Tables>) {
+  >(
+    options: DatabaseOptions<DbDialect, Tables>
+  ): SecureSqlDb<DbDialect, Tables>;
+  public static define<
+    DbDialect extends typeof Dialect.SQLITE,
+    Tables extends Record<string, Table<string, Record<string, Column>>>,
+  >(
+    options: DefineSecureDbOptions<DbDialect, Tables>
+  ): Promise<SecureSqlDb<DbDialect, Tables>>;
+  public static define<
+    DbDialect extends typeof Dialect.SQLITE,
+    Tables extends Record<string, Table<string, Record<string, Column>>>,
+  >(
+    options:
+      | DatabaseOptions<DbDialect, Tables>
+      | DefineSecureDbOptions<DbDialect, Tables>
+  ) {
     if (options.dialect !== Dialect.SQLITE) {
       throw new Error('Only SQLite is supported');
     }
 
     const config = options.config;
-    const password = options.password;
     const encryptedFilePath = config.filename + '.enc';
     const decryptedFilePath = config.filename;
     const metaPath = config.filename + '.meta.json';
-    const { isExists: isMetaExists, meta } = await readMeta(metaPath);
 
-    if (
-      !(await isFileExists(decryptedFilePath)) &&
-      (await isFileExists(encryptedFilePath))
-    ) {
-      await decryptFile({
-        input: encryptedFilePath,
-        output: decryptedFilePath,
-        salt: options.salt,
-        password,
-      });
+    if ('password' in options) {
+      return readMeta(metaPath).then(
+        async ({ isExists: isMetaExists, meta }) => {
+          if (
+            !(await isFileExists(decryptedFilePath)) &&
+            (await isFileExists(encryptedFilePath))
+          ) {
+            await decryptFile({
+              input: encryptedFilePath,
+              output: decryptedFilePath,
+              ...options,
+            });
+          }
+
+          if (!isMetaExists) {
+            await writeMeta(metaPath, meta);
+          }
+
+          return new SecureSqlDb({
+            decryptedFilePath,
+            encryptedFilePath,
+            metaPath,
+            meta: meta,
+            ...options,
+          });
+        }
+      );
     }
-
-    if (!isMetaExists) {
-      await writeMeta(metaPath, meta);
-    }
-
-    const db = Database.define({
-      dialect: options.dialect,
-      tables: options.tables,
-      config,
-    });
 
     return new SecureSqlDb({
       decryptedFilePath,
       encryptedFilePath,
-      metaPath,
-      password,
-      meta: meta,
-      salt: options.salt,
-      db,
+      metaPath: '',
+      password: '',
+      salt: '',
+      meta: {} as DatabaseMeta,
+      ...options,
     });
   }
 }
--- packages/securedb/src/sql/types.ts
+++ packages/securedb/src/sql/types.ts
@@ -1,15 +1,12 @@
 import type { DatabaseOptions } from '@ignisia/sql/dist/database/types';
-import type { Column, Database, Table } from '@ignisia/sql';
+import type { Column, Table } from '@ignisia/sql';
 import type { Dialect } from '@ignisia/sql/dist/table/constants';
 import type { DatabaseMeta } from '../types';
 
 export interface SecureDbOptions<
-  Db extends Database<
-    typeof Dialect.SQLITE,
-    Record<string, Table<string, Record<string, Column>>>
-  >,
-> {
-  db: Db;
+  DbDialect extends typeof Dialect.SQLITE,
+  Tables extends Record<string, Table<string, Record<string, Column>>>,
+> extends DatabaseOptions<DbDialect, Tables> {
   decryptedFilePath: string;
   encryptedFilePath: string;
   metaPath: string;
@@ -18,10 +15,11 @@ export interface SecureDbOptions<
   salt?: string | null;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface DefineSecureDbOptions<
   DbDialect extends typeof Dialect.SQLITE,
   Tables extends Record<string, Table<string, Record<string, Column>>>,
-> extends DatabaseOptions<DbDialect, Tables> {
-  password: string;
-  salt?: string | null;
-}
+> extends Omit<
+    SecureDbOptions<DbDialect, Tables>,
+    'decryptedFilePath' | 'encryptedFilePath' | 'metaPath' | 'meta'
+  > {}
