1e8f096f06fcfaec64819536a9538c5d92b465f6
Muhammad Firdaus Sati <firdaus.sati51@gmail.com>
2025-04-12 08:27:37 +0700

    feat(core): add request headers accessor
    fix(core): change params to param to unified the functions naming

--- packages/core/src/context/request.ts
+++ packages/core/src/context/request.ts
@@ -6,7 +6,7 @@ export class ContextRequest<
   // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-constraint
   Values extends any = any,
   Params extends Record<string, string> = NonNullable<unknown>,
-  Query extends Record<string, string> = NonNullable<unknown>,
+  Query extends Record<string, string | string[]> = NonNullable<unknown>,
 > {
   private _request: Request;
   private _url: URL | null;
@@ -22,12 +22,12 @@ export class ContextRequest<
     this._query = null;
   }
 
-  public params(): Params;
-  public params<
+  public param(): Params;
+  public param<
     K extends keyof Params | (string & {}),
     V extends K extends keyof Params ? Params[K] : string,
   >(key: K): V;
-  public params<K extends keyof Params | (string & {})>(key?: K) {
+  public param<K extends keyof Params | (string & {})>(key?: K) {
     if (key) return this._params[key];
 
     return this._params;
@@ -36,16 +36,38 @@ export class ContextRequest<
   public query(): Query;
   public query<
     K extends keyof Query | (string & {}),
-    V extends K extends keyof Query ? Query[K] : string,
+    V extends K extends keyof Query ? Query[K] | null : string | null,
   >(key: K): V;
   public query<K extends keyof Query | (string & {})>(key?: K) {
     if (!this._query) {
       this._query = parseQuery(this.url.searchParams) as Query;
     }
 
-    if (key) return this._query[key];
+    if (key) return this._query[key] || null;
 
     return this._query;
+  }
+
+  public header(): Record<string, string>;
+  public header(key: string): string | null;
+  public header(key?: string) {
+    if (key) {
+      if (this._cache.headers) {
+        return this._cache.headers[key.toLowerCase()] || null;
+      }
+
+      return this._request.headers.get(key);
+    }
+
+    if (!this._cache.headers) {
+      this._cache.headers = {};
+
+      for (const [key, value] of this._request.headers.entries()) {
+        this._cache.headers[key.toLowerCase()] = value;
+      }
+    }
+
+    return this._cache.headers;
   }
 
   public async json<T extends Values = any>(): Promise<T> {
