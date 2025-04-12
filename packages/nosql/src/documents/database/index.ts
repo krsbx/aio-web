5d38e65f3b881b34fc79cd2fe0d55e1e4930f635
Muhammad Firdaus Sati <firdaus.sati51@gmail.com>
2025-04-13 00:02:09 +0700

    fix(nosql): after/before query hooks not being bind

--- packages/nosql/src/documents/database/index.ts
+++ packages/nosql/src/documents/database/index.ts
@@ -1,17 +1,21 @@
 import type { Document } from '../document';
 import type { Field } from '../field';
 import type { QueryBuilder } from '../query';
+import type { QueryHooksType } from '../query/constants';
+import type { QuerHooks, QueryRunHooks } from '../query/types';
 import type { DatabaseDialect, DatabaseOptions } from './types';
 import { DatabaseSqlite } from './wrapper';
 
 export class Database<
   Docs extends Record<string, Document<string, Record<string, Field>>>,
 > {
+  public readonly hooks: Partial<QuerHooks>;
   public readonly docs: Docs;
   public readonly filename: string;
   public readonly client: DatabaseDialect;
 
   protected constructor(options: DatabaseOptions<Docs>) {
+    this.hooks = {};
     this.docs = options.docs;
     this.filename = options.filename;
     this.client = new DatabaseSqlite(options);
@@ -47,8 +51,32 @@ export class Database<
     }
 
     const doc = this.docs[docName];
+    const query = doc.query();
 
-    return doc.query() as unknown as QueryBuilder<DocName, Doc>;
+    // Bind the hooks from the database class to the query
+    query.hooks.before = this.hooks.before;
+    query.hooks.after = this.hooks.after;
+
+    // Fix the type
+    return query as unknown as QueryBuilder<DocName, Doc>;
+  }
+
+  public addHook(type: QueryHooksType, fn: QueryRunHooks) {
+    if (!this.hooks[type]) {
+      this.hooks[type] = new Set();
+    }
+
+    this.hooks[type].add(fn);
+
+    return this;
+  }
+
+  public removeHook(type: QueryHooksType, fn: QueryRunHooks) {
+    if (this.hooks[type]) {
+      this.hooks[type].delete(fn);
+    }
+
+    return this;
   }
 
   public static define<
