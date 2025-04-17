8ea5e781ee5df5e3ffc0005aa2aa4055fcd079df
Muhammad Firdaus Sati <firdaus.sati51@gmail.com>
2025-04-17 16:38:26 +0700

    chore(forge): reuse core types/contants

--- packages/forge/src/types/api.ts
+++ packages/forge/src/types/api.ts
@@ -1,10 +1,6 @@
 import type { Context } from '@ignisia/core';
+import type { ApiMethod } from '@ignisia/core/app/constants';
 
-export interface ForgeApi {
-  GET?(ctx: Context): Promise<Response>;
-  POST?(ctx: Context): Promise<Response>;
-  PUT?(ctx: Context): Promise<Response>;
-  PATCH?(ctx: Context): Promise<Response>;
-  DELETE?(ctx: Context): Promise<Response>;
-  OPTIONS?(ctx: Context): Promise<Response>;
-}
+export type ForgeApi = {
+  [key in ApiMethod]?: (ctx: Context) => Promise<Response>;
+};
